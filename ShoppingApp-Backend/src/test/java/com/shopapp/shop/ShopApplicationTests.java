25094ff9f516db3775bbb53c140017f78a771e03
Manh Phan <imseirousmanh@gmail.com>
2025-03-24 20:50:14 +0700

    first commit

---
+++ ShoppingApp-Backend/src/test/java/com/shopapp/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopapp.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
