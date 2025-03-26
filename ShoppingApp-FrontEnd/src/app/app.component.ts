00a8907f0a5875381878f6cec800a38ec4c29c42
Manh Phan <imseirousmanh@gmail.com>
2025-03-26 19:46:01 +0700

    frontend-complete header-qr

--- ShoppingApp-FrontEnd/src/app/app.component.ts
+++ ShoppingApp-FrontEnd/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
