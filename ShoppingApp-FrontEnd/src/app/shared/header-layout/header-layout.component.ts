819da938158439f55a0c9aae06a972a90fe8bd24
Manh Phan <imseirousmanh@gmail.com>
2025-03-28 13:48:16 +0700

    FrontEnd complete authentication form+Navbar

--- ShoppingApp-FrontEnd/src/app/shared/header-layout/header-layout.component.ts
+++ ShoppingApp-FrontEnd/src/app/shared/header-layout/header-layout.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header-layout',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './header-layout.component.html',
-  styleUrl: './header-layout.component.css'
+  styleUrls: ['./header-layout.component.css'],
 })
 export class HeaderLayoutComponent {
+  isRegistering: boolean = false; // Mặc định hiển thị form đăng ký
 
+  toggleForm() {
+    console.log('toggleForm() được gọi');
+    this.isRegistering = !this.isRegistering;
+  }
 }
